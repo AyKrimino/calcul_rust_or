f906a70b2a0499ba5271be16cd848f77332e5b75
AyKrimino <ayoub.krimi14@gmail.com>
2026-05-09 21:42:29 +0100

    refactor: encapsulate calculator logic into Calculator struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,142 @@ use colored::*;
 use terminal_size::{Width, Height, terminal_size};
 use std::io::{self, Write};
 
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Exit,
+}
+
+struct Calculator {
+    terminal_width: usize,
+    app_name: String,
+    menu: Vec<String>,
+}
+
+impl Calculator {
+    fn new(width: usize, name: String, menu: Vec<String>) -> Self {
+        Self {
+            terminal_width: width,
+            app_name: name,
+            menu,
+        }
+    }
+
+    fn render_menu(&self) {
+        self.pretty_print(&self.app_name);
+        for m in self.menu.iter() {
+            self.pretty_print(m);
+        }
+    }
+
+    fn styled_message(text: &String) -> impl std::fmt::Display {
+        text
+            .bright_cyan()
+            .bold()
+            .on_black()
+    }
+
+    fn styled_error(text: &String) -> impl std::fmt::Display {
+        text
+            .red()
+            .bold()
+            .on_black()
+    }
+
+    fn pretty_print(&self, text: &String) {
+        println!(
+            "{:^width$}",
+            Calculator::styled_message(text),
+            width = self.terminal_width
+        );
+    }
+
+    fn pretty_print_inline(&self, text: &String) {
+        print!("{}", Calculator::styled_message(text));
+    }
+
+    fn pretty_print_error(&self, text: &String) {
+        println!("{}", Calculator::styled_error(text));
+    }
+
+    fn read_number(&self, prompt: &String) -> f64 {
+        loop {
+            self.pretty_print(prompt);
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+
+            match input.trim().parse::<f64>() {
+                Ok(num) => return num,
+                Err(_) => {
+                    self.pretty_print_error(&String::from("Please enter a valid number"));
+                }
+            }
+        }
+    }
+
+    fn get_two_numbers(&self) -> (f64, f64) {
+        let a = self.read_number(&String::from("Enter first number:"));
+        let b = self.read_number(&String::from("Enter second number:"));
+
+        (a, b)
+    }
+
+    fn start_loop(&self) {
+        loop {
+            self.pretty_print_inline(&String::from("Enter your choice: "));
+
+            io::stdout().flush().unwrap();
+
+            let mut option = String::new();
+
+            io::stdin().read_line(&mut option).expect("Failed to read line");
+
+            let option: i32 = match option.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    self.pretty_print_error(&String::from("Please type a number."));
+                    continue;
+                },
+            };
+
+            match option {
+                1 => {
+                    let (a, b) = self.get_two_numbers();
+                    self.pretty_print(&format!("{} + {} = {}", a, b, a + b));
+                },
+                2 => {
+                    let (a, b) = self.get_two_numbers();
+                    self.pretty_print(&format!("{} - {} = {}", a, b, a - b));
+                },
+                3 => {
+                    let (a, b) = self.get_two_numbers();
+                    self.pretty_print(&format!("{} * {} = {}", a, b, a * b));
+                },
+                4 => {
+                    let (a, b) = self.get_two_numbers();
+                    if b == 0.0 {
+                        self.pretty_print_error(&String::from("Cannot divide by zero"));
+                        continue;
+                    }
+                    self.pretty_print(&format!("{} / {} = {}", a, b, a / b));
+                },
+                5 => {
+                    self.pretty_print(&String::from("Exit"));
+                    break;
+                },
+                _ => {
+                    self.pretty_print_error(&String::from("Please type a number between 1 and 5"));
+                    continue
+                },
+            }
+        }
+    }
+}
+
 fn main() {
     const NAME: &str = "CALCULATOR PROGRAM";
 
@@ -11,114 +147,17 @@ fn main() {
         80
     };
 
-    let menu = [
-        NAME,
-        "1) Addition",
-        "2) Subtraction",
-        "3) Multiplication",
-        "4) Division",
-        "5) Exit",
+    let menu = vec![
+        String::from("1) Addition"),
+        String::from("2) Subtraction"),
+        String::from("3) Multiplication"),
+        String::from("4) Division"),
+        String::from("5) Exit"),
     ];
 
-    for m in menu.iter() {
-        println!(
-            "{:^width$}\n",
-            m
-            .bright_cyan()
-            .bold()
-            .on_black(),
-            width = width
-        );
-    }
+    let calculator = Calculator::new(width, String::from(NAME), menu.clone());
 
-    loop {
-        print!(
-            "{}",
-            "Enter your choice: "
-            .bright_cyan()
-            .bold()
-            .on_black(),
+    calculator.render_menu();
 
-        );
-        io::stdout().flush().unwrap();
-
-        let mut option = String::new();
-
-        io::stdin().read_line(&mut option).expect("Failed to read line");
-
-        let option: i32 = match option.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("{}","Please type a number".red());
-                continue;
-            },
-        };
-
-        match option {
-            1 => {
-                let (a, b) = get_two_numbers();
-                println!("{}", format!("{} + {} = {}", a, b, a + b).bright_cyan().bold().on_black())
-            },
-            2 => {
-                let (a, b) = get_two_numbers();
-                println!("{}", format!("{} - {} = {}", a, b, a - b).bright_cyan().bold().on_black())
-            },
-            3 => {
-                let (a, b) = get_two_numbers();
-                println!("{}", format!("{} * {} = {}", a, b, a * b).bright_cyan().bold().on_black())
-            },
-            4 => {
-                let (a, b) = get_two_numbers();
-                if b == 0.0 {
-                    println!("{}", "Cannot divide by zero".red());
-                    continue;
-                }
-                println!("{}", format!("{} / {} = {}", a, b, a / b).bright_cyan().bold().on_black())
-            },
-            5 => {
-                println!("{}", "Exit");
-                break;
-            },
-            _ => {
-                println!("{}","Please type a number between 1 and 5".red());
-                continue
-            },
-        }
-    }
-}
-
-fn get_two_numbers() -> (f64, f64) {
-    use colored::*;
-    use std::io::{self, Write};
-
-    fn read_number(prompt: &str) -> f64 {
-        loop {
-            print!(
-                "{}",
-                prompt.bright_cyan().bold().on_black(),
-            );
-            io::stdout().flush().unwrap();
-
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-
-            match input.trim().parse::<f64>() {
-                Ok(num) => return num,
-                Err(_) => {
-                    println!(
-                        "{}",
-                        "Please enter a valid number"
-                            .red()
-                            .bold()
-                            .on_black(),
-                    );
-                }
-            }
-        }
-    }
-
-    let a = read_number("Enter first number:");
-    let b = read_number("Enter second number:");
-
-    (a, b)
+    calculator.start_loop();
 }
